595920b6ee1ae43cbdfe4f135c76ee4696644879
Ubuntu <ubuntu@ip-172-31-30-64.ec2.internal>
2024-06-18 01:57:19 +0000

    Successful Install

--- config.example.php
+++ config.example.php
@@ -4,31 +4,31 @@
 date_default_timezone_set('UTC');
 
 // Application name
-define('APP_NAME', 'Tripwire');
+define('APP_NAME', 'Northstar');
 
 // Content file server (Use CDN here if you have one) - used for serving images, css, js files
-define('CDN_DOMAIN', 'localhost');
+define('CDN_DOMAIN', 'cdfns.ddns.net');
 
 // EVE SDE table name
-define('EVE_DUMP', 'eve_carnyx');
+define('EVE_DUMP', 'eve_sde');
 
 // Enable Tripwire API?
 define('TRIPWIRE_API', false);
 
 // EVE API userAgent
-define('USER_AGENT', 'Tripwire Server - adminEmail@example.com');
+define('USER_AGENT', 'Northstar Server - adminEmail@example.com');
 
 // EVE SSO info
 define('EVE_SSO_CLIENT', 'clientID');
 define('EVE_SSO_SECRET', 'secret');
-define('EVE_SSO_REDIRECT', 'http://localhost/index.php?mode=sso');
+define('EVE_SSO_REDIRECT', 'http://cdfns.ddns.net/index.php?mode=sso');
 
 // As of 2022-07-12, searching via the ESI API uses an endpoint that requires an
 // authentication token. In order to use the new search functionality, you will
 // need to add the `esi-search.search_structures.v1` scope to your application
 // in the `developers.eveonline.com` portal. Once you have done that, this
 // value can be changed from 'false' to 'true'.
-define('ENABLE_SEARCH_SCOPE', false);
+define('ENABLE_SEARCH_SCOPE', true);
 
 // Discord integration
 /*define('DISCORD_WEB_HOOK', array(
